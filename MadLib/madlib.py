a62435f0368360f7f2b6ae84a004f9f31fee70e7
Angelena Ward <angelena@fullsail.edu>
2015-05-06 12:28:20 -0400

    dictionary added

--- MadLib/madlib.py
+++ MadLib/madlib.py
@@ -3,6 +3,7 @@ Name: Angelena Ward
 Date: May 6, 2015
 '''
 
+#array to hold input from user
 numbers = []
 
 #variables
@@ -23,6 +24,15 @@ year = raw_input("Enter the year you were born. ")
 numbers.append(raw_input("Enter a number from 1 to 15. "))
 numbers.append(raw_input("Enter any number. "))
 numbers.append(raw_input("Enter any number. "))
+
+#dictionary
+activity = dict()
+activity = {"fun": "road trip", "boring": "jogging"}
+
+
+print "Hi, my name is "+name +" and I am " + str(new_age) + " years old!"
+print "We decided it might be more fun to go on a " + activities["fun"] + " than it would be to go " + activities["boring"]
+
 
 message = '''My name is {name} and my friends {male}, {female} and I went on a road trip to {state}. "You will love {state}" said {male} and I did. "It's famous for its wild {animal}", the {color} buildings downtown, and it's {verb} hills." I packed plenty of {food} for the ride, it will probably take {numbers[0]} hours to get there. On the car ride down we sang {numbers[1]} bottles of {liquid} on the wall, then we counted the number of {noun} we saw {verb1} on the side of the road, and we counted {numbers[2]} {noun} {verb1}. This was a great trip.'''
 message = message.format(**locals())
