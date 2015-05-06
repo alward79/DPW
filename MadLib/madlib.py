727934aebab85de9e1b66ed3b9e8ee2d9a0e9cbb
Angelena Ward <angelena@fullsail.edu>
2015-05-06 12:12:45 -0400

    story added

--- MadLib/madlib.py
+++ MadLib/madlib.py
@@ -19,8 +19,11 @@ verb1 = raw_input("Enter another verb ending in ing! ")
 noun = raw_input("Enter any noun! (plural) ")
 year = raw_input("Enter the year you were born. ")
 
+#append user input into numbers array
 numbers.append(raw_input("Enter a number from 1 to 15. "))
 numbers.append(raw_input("Enter any number. "))
 numbers.append(raw_input("Enter any number. "))
 
-print numbers
+message = '''My name is {name} and my friends {male}, {female} and I went on a road trip to {state}. "You will love {state}" said {male} and I did. "It's famous for its wild {animal}", the {color} buildings downtown, and it's {verb} hills." I packed plenty of {food} for the ride, it will probably take {numbers[0]} hours to get there. On the car ride down we sang {numbers[1]} bottles of {liquid} on the wall, then we counted the number of {noun} we saw {verb1} on the side of the road, and we counted {numbers[2]} {noun} {verb1}. This was a great trip.'''
+message = message.format(**locals())
+print message
