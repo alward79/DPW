3f20a8c59f2ea17b7f1d813878f80e4981b017d5
Angelena Ward <angelena@fullsail.edu>
2015-05-06 06:12:04 -0400

    more intro

--- intro to python/main.py
+++ intro to python/main.py
@@ -36,4 +36,49 @@ characters.append("obi won")
 movies = dict() #create dictionary object
 movies = {"Star Wars":"Darth Vader", "Silence of the Lambs":"Hannibal Lecter"}
 
-print movies ["Star Wars"]
+#print movies ["Star Wars"]
+
+
+#while loop
+'''
+i = 0
+while i <9:
+    print "The count is", i
+    i = i+1
+'''
+#for Loop
+'''
+for i in range (0,10):
+    print "The count is", i
+    i = i+i
+'''
+#foreach loop
+rappers = ["Tupac", "Nas","Biggis Smalls"]
+for r in rappers:
+    #print "One of the best rappers is " + r
+    pass
+
+#functions
+x = 2
+def calcArea(h, w):
+    area = h * w
+    return area + x
+
+a = calcArea(20, 40);
+#print "My area is "+  str(a) + "sqft"
+print a
+
+title = "Contact Us"
+body = "You can contact us at contact@us.com"
+message = '''
+<DOCTYPE HTML>
+    <head>
+        <title>{title}</title>
+    </head>
+    <body>
+        {body}
+    </body>
+</html>
+'''
+message = message.format(**locals())
+print message
