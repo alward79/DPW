f66342c8980f88f9b44c2b91544afbe441aa0d56
Angelena Ward <angelena@fullsail.edu>
2015-05-06 10:47:39 -0400

    variables added

--- MadLib/madlib.py
+++ MadLib/madlib.py
@@ -2,3 +2,25 @@
 Name: Angelena Ward
 Date: May 6, 2015
 '''
+
+numbers = []
+
+#variables
+female = raw_input("Enter a female name. ")
+male = raw_input("Enter a male name. ")
+name = raw_input("Enter your name. ")
+state = raw_input("Enter your state. ")
+color = raw_input("Enter your favorite color. ")
+animal = raw_input("Name an animal. ")
+food = raw_input("Enter your favorite food. ")
+liquid = raw_input("Enter a liquid. ")
+verb = raw_input("Enter any verb ending in ing! ")
+verb1 = raw_input("Enter another verb ending in ing! ")
+noun = raw_input("Enter any noun! (plural) ")
+year = raw_input("Enter the year you were born. ")
+
+numbers.append(raw_input("Enter a number from 1 to 15. "))
+numbers.append(raw_input("Enter any number. "))
+numbers.append(raw_input("Enter any number. "))
+
+print numbers
