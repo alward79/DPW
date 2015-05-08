b8daaf06ec161f7d4794d90bb3006ef50d592200
Angelena Ward <angelena@fullsail.edu>
2015-05-07 21:49:32 -0400

    launcher setup

--- simple-login/main.py
+++ simple-login/main.py
@@ -1,10 +1,16 @@
+'''
+Angelena Ward
+May 7
+Design Patterns for Web Programming
+practice
+'''
+import webapp2 #use the webapp2 library
 
-import webapp2
-
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
+class MainHandler(webapp2.RequestHandler):#declaring a class
+    def get(self): #funciton that starts everything
         self.response.write('Hello world!')
 
+#nver touch this part of whats making python work within browser
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
