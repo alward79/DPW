5688794568476f43efad404b502a288e49ebc263
Angelena Ward <angelena@fullsail.edu>
2015-05-21 17:57:05 -0400

    variable calling Jewelry class

--- reusablelibrary/main.py
+++ reusablelibrary/main.py
@@ -5,10 +5,12 @@ Design Patterns for Web Programming
 5/21/2015
 '''
 import webapp2
+from lib import Jewelry
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        #calling Jewelry() class from lib.py
+        j = Jewelry()
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
