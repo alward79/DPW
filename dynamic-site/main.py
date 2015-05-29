9b4a03ca0ddd99943238181053f3f20445d04e74
Angelena Ward <angelena@fullsail.edu>
2015-05-29 12:16:21 -0400

    imports added to main handler

--- dynamic-site/main.py
+++ dynamic-site/main.py
@@ -6,10 +6,15 @@ Dynamic Site
 '''
 
 import webapp2
+from pages import Content #imports from pages.py the Content() class
+from data import Data #import from data.py the Data() class
 
+#MainHandler() receives and handles request
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        #refencing the subclasses from pages.py and data.py
+        page = Content()
+        data = Data()
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
