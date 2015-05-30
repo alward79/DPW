77cbcfeddaeef537614859da029218d2811fca44
Angelena Ward <angelena@fullsail.edu>
2015-05-29 22:55:47 -0400

    images and team names updated

--- dynamic-site/data.py
+++ dynamic-site/data.py
@@ -46,7 +46,7 @@ class Data(object):
 
         washington = TopTeams() #attribute to store values for washington
         washington.team_name = 'Washington Wizards'
-        washington.img = 'http://sportsradio1340.info/images/wizards.png'
+        washington.img = 'http://upload.wikimedia.org/wikipedia/en/thumb/b/bd/Washington_Wizards_Logo.svg/1111px-Washington_Wizards_Logo.svg.png'
         washington.wins = 46
         washington.losses = 36
         washington.home_game = 29 - 12
@@ -54,40 +54,40 @@ class Data(object):
 
         #western team instance
         golden_state = TopTeams()#attribute to store values for golden state
-        golden_state.team_name = 'Atlanta Hawks'
-        golden_state.img = 'http://i0.wp.com/stuffled.com/wp-content/uploads/2014/04/Atlanta-Hawks-Logo-Vector-Image.png?resize=1020%2C680'
+        golden_state.team_name = 'Golden State Warriors'
+        golden_state.img = 'http://clippertalk.com/sites/default/files/logos/Warriors.png'
         golden_state.wins = 67
         golden_state.losses = 15
         golden_state.home_game = 39 - 2
         golden_state.road_game = 28 -13
 
         houston = TopTeams() #attribute to store values for houston
-        houston.team_name = 'Atlanta Hawks'
-        houston.img = 'http://i0.wp.com/stuffled.com/wp-content/uploads/2014/04/Atlanta-Hawks-Logo-Vector-Image.png?resize=1020%2C680'
+        houston.team_name = 'Houston Rockets'
+        houston.img = 'http://upload.wikimedia.org/wikipedia/de/thumb/2/28/Houston_Rockets.svg/2000px-Houston_Rockets.svg.png'
         houston.wins = 56
         houston.losses = 26
         houston.home_game = 30 -11
         houston.road_game = 26 -15
 
         clippers = TopTeams() #attribute to store values for LA clippers
-        clippers.team_name = 'Atlanta Hawks'
-        clippers.img = 'http://i0.wp.com/stuffled.com/wp-content/uploads/2014/04/Atlanta-Hawks-Logo-Vector-Image.png?resize=1020%2C680'
+        clippers.team_name = 'Los Angeles Clippers'
+        clippers.img = 'http://www.nightmovesonline.com/wp-content/uploads/2014/04/Los-Angeles-Clippers-Logo-Medium.png'
         clippers.wins = 56
         clippers.losses = 26
         clippers.home_game = 30 - 11
         clippers.road_game = 26 - 15
 
         portland = TopTeams() #attribute to store values for portland
-        portland.team_name = 'Atlanta Hawks'
-        portland.img = 'http://i0.wp.com/stuffled.com/wp-content/uploads/2014/04/Atlanta-Hawks-Logo-Vector-Image.png?resize=1020%2C680'
+        portland.team_name = 'Portland Trail Blazers'
+        portland.img = 'http://img1.wikia.nocookie.net/__cb20101026151711/nba/images/b/b7/Portland_Trail_Blazers_alternate_logo.png'
         portland.wins = 51
         portland.losses = 31
         portland.home_game = 32 - 9
         portland.road_game = 19 - 22
 
         memphis = TopTeams() #attribute to store values for memphis
-        memphis.team_name = 'Atlanta Hawks'
-        memphis.img = 'http://i0.wp.com/stuffled.com/wp-content/uploads/2014/04/Atlanta-Hawks-Logo-Vector-Image.png?resize=1020%2C680'
+        memphis.team_name = 'Memphis Grizzlies'
+        memphis.img = 'http://upload.wikimedia.org/wikipedia/fr/6/6f/Memphis_Grizzlies_logo.png'
         memphis.wins = 55
         memphis.losses = 27
         memphis.home_game = 31 - 10
